ccdab9178e357e75d8b291598fb52349cbe00ff5
Fabio Siliberto <fabio.siliberto@gmail.com>
2021-11-21 18:23:28 +0100

    Use primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule } from 'primeng/menubar';
+import { SharedModule } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MenubarModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
