e071099ddfd6f3fccf36cec91dde9dfb18ee2897
Fabio Siliberto <fabio.siliberto@gmail.com>
2022-05-01 21:24:31 +0200

    Add list view

--- src/app/home/calendar-header/calendar-header.component.ts
+++ src/app/home/calendar-header/calendar-header.component.ts
@@ -1,25 +1,32 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CalendarView } from 'angular-calendar';
 
+export enum SRCCalendarView {
+  List
+}
+
+export type ExtendedCalendarView = CalendarView | SRCCalendarView;
+
 @Component({
   selector: 'app-calendar-header',
   templateUrl: 'calendar-header.component.html',
   styleUrls: ['calendar-header.component.scss']
 })
 export class CalendarHeaderComponent {
-  @Input() view!: CalendarView;
+  @Input() view!: ExtendedCalendarView;
 
   @Input() viewDate!: Date;
 
-  @Output() viewChange = new EventEmitter<CalendarView>();
+  @Output() viewChange = new EventEmitter<ExtendedCalendarView>();
 
   @Output() viewDateChange = new EventEmitter<Date>();
 
-  CalendarView = CalendarView;
+  SRCCalendarView = SRCCalendarView;
 
   viewsOptions = [
     {label: 'Month', value: CalendarView.Month},
     {label: 'Week', value: CalendarView.Week},
-    {label: 'Day', value: CalendarView.Day}
+    {label: 'Day', value: CalendarView.Day},
+    {label: 'List', value: SRCCalendarView.List}
   ]
 }
