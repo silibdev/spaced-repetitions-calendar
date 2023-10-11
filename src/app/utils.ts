f759994bd1099305f2ed10ab6c76a554765586e6
Fabio Siliberto <fabio.siliberto@gmail.com>
2023-10-11 17:35:14 +0200

    Test usage new sound system

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -2,15 +2,15 @@ import { v4 } from 'uuid';
 
 export class Utils {
   static playAudio(audio: HTMLAudioElement | undefined) {
-    if (audio) {
-      audio.pause();
-      audio.currentTime = 0;
-      return new Promise(resolve => {
-        audio.play();
-        audio.addEventListener("ended", () => resolve(undefined));
-      });
+    console.log('playing', audio);
+    if (!audio) {
+      return Promise.resolve();
     }
-    return Promise.resolve();
+    Utils.stopAudio(audio);
+    return new Promise(resolve => {
+      audio.play();
+      audio.addEventListener("ended", () => resolve(undefined));
+    });
   }
 
   static generateRandomUUID(): string {
@@ -19,5 +19,14 @@ export class Utils {
 
   static generateRandomColor(): string {
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  static stopAudio(audio: HTMLAudioElement | undefined) {
+    console.log('stop', audio);
+    if (!audio) {
+      return;
+    }
+    audio.pause();
+    audio.currentTime = 0;
   }
 }
